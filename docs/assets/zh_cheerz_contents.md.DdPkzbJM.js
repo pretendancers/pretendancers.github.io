f88ec7cbb04358fdb7320d6a7bce09726c1c0468
Canned Lychee <cannedlychee@unknown26cb56cf8006.ctc>
2025-05-27 16:57:31 +0800

    new deploy

---
+++ docs/assets/zh_cheerz_contents.md.DdPkzbJM.js
@@ -0,0 +1,26 @@
+import{_ as o,I as i,c as t,o as p,af as e,j as r,J as s,a,w as d}from"./chunks/framework.CPLsEXPO.js";const P=JSON.parse('{"title":"包装清单","description":"","frontmatter":{},"headers":[],"relativePath":"zh/cheerz/contents.md","filePath":"zh/cheerz/contents.md","lastUpdated":1748219874000}'),g={name:"zh/cheerz/contents.md"};function m(c,l,_,x,u,T){const n=i("font");return p(),t("div",null,[l[6]||(l[6]=e(`<h1 id="包装清单" tabindex="-1">包装清单 <a class="header-anchor" href="#包装清单" aria-label="Permalink to “包装清单”">​</a></h1><p>拆开塑封抽出封套，翻开磁吸盖后，包装内应有以下内容：</p><h4 id="一、主体" tabindex="-1">一、主体 <a class="header-anchor" href="#一、主体" aria-label="Permalink to “一、主体”">​</a></h4><ol><li><p><strong>键盘外壳</strong></p><pre><code> 上下盖拧好螺丝塑封
+</code></pre><p><img src="" alt=""></p><pre><code> 已经预安装：C口小板/短排线/磁吸小板/磁吸盖板/上盖匀光件
+</code></pre><p><img src="" alt=""></p></li><li><p><strong>PCB母板</strong></p><pre><code> 静电袋包装
+</code></pre><p><img src="" alt=""></p></li><li><p><strong>定位板</strong></p><pre><code> 自封袋包装，有通用、直列、斜列三种
+</code></pre><p><img src="" alt=""></p></li><li><p><strong>内胆棉</strong></p><ul><li><p><strong>包装</strong><img src="" alt=""></p></li><li><p><strong>底棉</strong><img src="" alt=""></p></li><li><p><strong>轴座棉</strong><img src="" alt=""></p></li><li><p><strong>轴下垫</strong><img src="" alt=""></p></li><li><p><strong>夹心棉</strong></p><pre><code>  跟随定位板不同区分兼容、直列、斜列三种
+</code></pre><p><img src="" alt=""></p></li></ul></li></ol><h4 id="二、配件" tabindex="-1">二、配件 <a class="header-anchor" href="#二、配件" aria-label="Permalink to “二、配件”">​</a></h4>`,5)),r("ol",null,[l[5]||(l[5]=e(`<li><p><strong>自封袋1</strong></p><ul><li><p><strong>M2内六角螺丝</strong> ×14 （备用）</p><pre><code>  内部螺丝，全部备用
+</code></pre></li><li><p><strong>M3内六角螺丝</strong> ×8 （备用）</p><pre><code>  上下盖螺丝，全部备用
+</code></pre></li></ul></li><li><p><strong>自封袋2</strong></p><ul><li><p><strong>硅胶粒</strong> ×12 （8/12备用）</p><pre><code>  用于gas，其中8个备用
+</code></pre></li><li><p><strong>遮光棉</strong> x3 （2/3备用）</p><pre><code>  环形，背胶，其中2个备用
+  贴在定位板中心孔周围（详见安装指南）
+</code></pre></li><li><p><em><strong>短排线</strong></em> x3 （备用）</p><pre><code>  已经预安装，全部备用
+  16.5mm，8pin反向，0.5mm间距，用于c口小板和磁吸小板连接
+</code></pre></li></ul></li><li><p><strong>自封袋3</strong></p><ul><li><p><strong>锂电池</strong> ×2</p><pre><code>  3.7V, 1400mAh, 5.18Wh
+  60mm x 32mm x 5mm，线长25mm
+  可选安装
+</code></pre></li></ul></li>`,3)),r("li",null,[l[4]||(l[4]=r("p",null,[r("strong",null,"自封袋4")],-1)),r("ul",null,[r("li",null,[l[1]||(l[1]=r("p",null,[r("strong",null,"长排线"),a(" x2 （备用）")],-1)),s(n,{color:"Red"},{default:d(()=>l[0]||(l[0]=[r("b",null," !!长排线请勿连接磁吸小板!! ",-1)])),_:1,__:[0]}),l[2]||(l[2]=r("pre",null,[r("code",null,`  60mm，8pin反向，0.5mm间距
+  备用，有线连接c口小板时使用
+`)],-1))]),l[3]||(l[3]=e(`<li><p><strong>磁吸盖板</strong> ×1 （备用）</p><pre><code>  黑色fr4
+</code></pre></li><li><p><strong>磁吸小板底棉</strong> x1 （备用）</p><pre><code>  条状，背胶
+</code></pre></li><li><p><strong>脚贴</strong> 黑色x8，白色x8</p><pre><code>  直径12mm x 厚度2mm，背胶，可自行选用
+</code></pre></li>`,3))])])]),l[7]||(l[7]=e(`<h4 id="三、其他" tabindex="-1">三、其他 <a class="header-anchor" href="#三、其他" aria-label="Permalink to “三、其他”">​</a></h4><ul><li><p><strong>1.25mm磁吸内六角扳手</strong> ×1</p><pre><code>  若磁性较弱，可以先用磁铁吸附磁化
+  用于M2螺丝
+</code></pre></li><li><p><strong>2.5mm磁吸内六角扳手</strong> ×1</p><pre><code>  若磁性较弱，可以先用磁铁吸附磁化
+  用于M3螺丝
+</code></pre></li><li><p><strong>Cheerz和纸胶带</strong> ×1</p></li><li><p><strong>PCB轴体安装参考卡</strong> x1</p><pre><code>  用于轴体安装参考，背面印有本站二维码
+</code></pre></li></ul><h1 id="汇总" tabindex="-1">汇总 <a class="header-anchor" href="#汇总" aria-label="Permalink to “汇总”">​</a></h1><ul><li>上下盖 x1</li><li>PCB x1</li><li>定位板 x1</li><li>棉（包括底棉/轴座棉/轴下垫/夹心棉 各1）</li><li>M2内六角螺丝 x14 - 全部备用</li><li>M3内六角螺丝 x8 - 全部备用</li><li>硅胶粒 x8 - 其中8粒备用</li><li>遮光棉 x3 - 其中2片备用</li><li>短排线 x3 - 全部备用</li><li>锂电池 x2 - 自行选用</li><li>长排线 x2 - 全部备用</li><li>磁吸盖板 x1 - 备用</li><li>磁吸小板底棉 x1 - 备用</li><li>胶贴 黑x8 白x8 - 自行选用</li><li>1.25mm 磁吸内六角扳手 x1</li><li>2.5mm 磁吸内六角扳手 x1</li><li>Cheerz 和纸胶带 x1</li><li>PCB轴体安装参考卡 x1</li></ul>`,4))])}const f=o(g,[["render",m]]);export{P as __pageData,f as default};
